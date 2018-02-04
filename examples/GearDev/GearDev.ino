2b143dcaae68fa5fe5c2c5e373b48345d88c92d1
wichian toso <dr.chian@hotmail.com>
2018-02-04 12:14:27 +0700

    Add files via upload

--- examples/GearDev/GearDev.ino
+++ examples/GearDev/GearDev.ino
@@ -1,28 +1,32 @@
-//created date Nov 01 2017
+//created date Nov 01, 2017
+//success date Nov 25, 2017
 #include <ArduinoJson.h>
+#include <EEPROM.h>
+
+//uncomment below to use Arduino Board as IO Pins
+//#define ARDUINO_AS_IO
 
 #define SERIAL_BAUD_RATE      115200    //Serial_Baud_Rate
 #define HTTP_PORT             80        //HTTP port for WebServer
+#define TCP_PORT              1988      //TCP port for socket
 #define DEVICE_NAME           "GearDev" //HostName
-#define CLEAR_SETTING_TRICKER D1        //Clear Setting Tricker,common use with Reset pin
-#define ARDUINO_VIRTUAL_RESET 11        //Assigned Digital Pin 11 for Reset pin (for Arduino Boards)
 
 boolean canRungear      =     false;
-boolean stringComplete  =     false;    // whether the string is complete
-String inputString      =     "";       // a String to hold incoming data
+boolean stringComplete  =     false;    // whether the string is complete on Serial
+String inputString      =     "";       // a String to hold incoming data from Serial
 
-unsigned long previousMillis = 0, pMillis = 0,pRunning=0;
+unsigned long previousMillis = 0, pMillis = 0, pRunning = 0;
 const long interval = 5000;
 unsigned int nextTime = 0;
 
 /*esp มีหน้าที่อัพเดท STA ไปให้บอร์ด arduino ไม่ให้ arduino เช็คว่า STA มีค่าเป็น 0
-  บอร์ด Arduino จะวิเคราะห์ ถ้า STA มีค่า เป็น 0 แสดงว่า esp8266 ไม่ยอส่งสวยหรือบกพร่องในการทำงาน
-  Arduino ก็จะทำการประหาร esp8266 ทันที เพื่อให้ esp8266 ทำงานเป็นปกติเหมือนเดิมนั่นเอง
+  บอร์ด Arduino จะวิเคราะห์ ถ้า STA มีค่า เป็น 0 แสดงว่า esp8266 ไม่ยอมส่งสวยหรือบกพร่องในการทำงาน
+  Arduino ก็จะทำการประหาร esp8266 ทันที เพื่อให้ esp8266 เกิดใหม่หรือทำงานเป็นปกติเหมือนเดิมนั่นเอง
 */
-int sta_running_duration = (1000*30); // 5 นาที
+int sta_running_duration = (1000 * 60) * 5; // 5 นาที
 
 typedef struct Device {
-  char label[10];//ชื่อที่จะให้แสดง
+  PGM_P label;   //ชื่อที่จะให้แสดง
   byte reserve;  //สงวนไว้ใช้ในอนาคต (ยังไม่ได้ใช้)
   byte cursta;   //สถานะปัจจุบันของอุปกรณ์
   byte pins[4];  //ขาสัญญาณทั้งหมดที่ใช้ในแต่ละแชนแนล (รองรับได้ไม่เกิน 4)
@@ -32,16 +36,62 @@ typedef struct Device {
    ดังนั้นในการควบคุมอุปกรณ์ไฟฟ้า Message
    ต้องเขียนในรูปบบตาม >> อ่านได้ในหน้า Readme
 */
-Device dev[6] = {
-  /*{label,   N/A, cursta, pin_used}*/
+#if defined(ARDUINO_AS_IO)
+Device dev[7] = {
+  //{label,N / A, cursta, pin_used}
   {"Front",  0,    0,     2}, //ใช้ขา 2
   {"Inside", 0,    0,     3}, //     3
   {"Back",   0,    0,     4}, //     4
   {"Toilet", 0,    0,     5}, //     5
-  {"FAN-A",  0,    0,    {6, 7, 8}}, // 6 7 8 สำหรับพัดลมหรืออุปกรณ์ที่ปรับความเร็วได้ 3 ระดับ L/M/H (ต้องใช้ขาสัญญาณ 3 ขา)
-  {"BedRoom", 0,    0,    {22, 23}} // 22 23 สำหรับพัดลมหรืออุปกรณ์ที่ปรับความเร็วได้ 2 ระดับ L/H (ที่ต้องใช้ขาสัญญาณ 2 ขา)
+  {"BedRoom",0,    0,    12}, //    12
+  {"FAN-A",  0,    0,    {6,  7, 8}},// 6 7 8 สำหรับพัดลมหรืออุปกรณ์ที่ปรับความเร็วได้ 3 ระดับ L/M/H (ต้องใช้ขาสัญญาณ 3 ขา)
+  {"Flash Light 2D", 0,    0,   {22, 23}}   // 22 23 สำหรับพัดลมหรืออุปกรณ์ที่ปรับความเร็วได้ 2 ระดับ L/H (ที่ต้องใช้ขาสัญญาณ 2 ขา)
 };
+#else
 
+const char naabaan[22] PROGMEM  = { //"ไฟตาแมว"
+    0xE0,0xB9,0x84,0xE0,0xB8,0x9F,0xE0,0xB8,0x95,0xE0,0xB8,0xB2,0xE0,0xB9,0x81,0xE0,0xB8,0xA1,0xE0,0xB8,0xA7,0x00
+};
+const char hero_fan[31] PROGMEM   = {//"พัดลมฮีโร่"
+    0xE0,0xB8,0x9E,0xE0,0xB8,0xB1,0xE0,0xB8,0x94,0xE0,0xB8,0xA5,0xE0,0xB8,0xA1,0xE0,0xB8,0xAE,0xE0,0xB8,0xB5,0xE0,0xB9,0x82,0xE0,0xB8,0xA3,0xE0,0xB9,0x88,0x00
+};
+Device dev[2] = {
+  //หลีกเลี่ยงการใช้ขา D3
+  //not working with pin D3, D3 mapped to pin GPIO0, 0 หรือ null byte ถูกใช้เป็นรหัสปิดท้ายข้อมูลในหน่วยความจำ
+  //warning! D4 reserver for LED Indicator on ESP8266Webserver
+  /*{label,   N/A, cursta, pin_used}*/
+  {naabaan,  0,    0,     D0},
+  {hero_fan, 0,    0,     {D2, D5, D6}}
+};
+#endif
+//ตำแหน่ง EEPROM ที่จะให้กู้คืนค่าสถานะขาสัญญาณ
+#define PREVIOS_STATUS_ADDRESS 2
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+
+
+#include "Button.h"
+
+//สำหรับนำไปทริกขารีเซ็ตของ ESP8266
+#define RESET_TRICKER  10
+Button resetSwitch(18);
+//สำหรับนำไปทริกขาเคลีร์ยการตั้งค่า
+#define CLEAR_SETTING_TRICKER  11
+
+int STA = 1;
+bool espboot = false;
+#include "pitches.h"
+#else
+#define CLEAR_SETTING_TRICKER D1        //Clear Setting Tricker,common use with Reset pin
+#endif
+#ifdef ARDUINO_ESP8266_NODEMCU
+#include <WebSocketsServer.h>
+
+//for socket
+//WiFiServer socketServer(TCP_PORT);
+//WiFiClient socketClient;
+WebSocketsServer webSocket = WebSocketsServer(TCP_PORT);
+#endif
 #include "MyFunction.h"
 
 #ifdef ARDUINO_ESP8266_NODEMCU
@@ -54,12 +104,15 @@ Device dev[6] = {
 //WiFiManager
 //Local intialization. Once its business is done, there is no need to keep it around
 WiFiManager wifiManager;
-WiFiClient client;
+WiFiClient mgearClient;
+
+
+
 //for GUI Webpage
-ESP8266WebServer server(HTTP_PORT);
+ESP8266WebServer webServer(HTTP_PORT);
 //for background Service
 //ESP8266WebServer service(8888);
-MicroGear microgear(client);
+MicroGear microgear(mgearClient);
 
 DynamicJsonBuffer json;
 
@@ -109,21 +162,29 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
       if (intVal >= 0 && intVal <= 255) {
         for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++) {
           dev[i].cursta = intVal;
+#ifndef ARDUINO_AS_IO
+          setPinsValue(i);
+#endif
         }
       }
       Serial.println("a=" + strVal);
-      microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#a=" + strVal);
+      //uncomment below 1 line if sending data to alias
+      //microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#a=" + strVal);
+      //uncomment below 1 line if sending data to topic
+      microgear.publish("/chat", String(wifiManager.mgear.Alias) + "#" + String(sender) + "#a=" + strVal,true);
       //dumpioIndex();
       return;
     }
   } else if (body.startsWith("?")) {
     body = "all=";
     for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++) {
-      body += dev[i].cursta;
+      body += String(getAvailablePins(dev[i].pins))+":"+dev[i].cursta;
       if (i < sizeof(dev) / sizeof(dev[0]) - 1)body += ",";
     }
     Serial.println("?");
-    microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body);
+    //microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body);
+    microgear.publish("/chat", String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body,true);
+    
     return;
   } else if (body.indexOf("=") > 0) {
     String strCh = splitString(body, '=', 0);
@@ -133,13 +194,21 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
       if (isValidNumber(strVal)) {
         int intVal = strVal.toInt();
         dev[intCh].cursta = intVal;
+#ifndef ARDUINO_AS_IO
+        setPinsValue(intCh);
+#endif
         Serial.println(body);
-        microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body);
+        //microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body);
+        microgear.publish("/chat", String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + body,true);
         return;
       } else if (strVal.equals("!")) {
         dev[intCh].cursta = map(dev[intCh].cursta, 0, 255, 255, 0);
+#ifndef ARDUINO_AS_IO
+        setPinsValue(intCh);
+#endif
         Serial.print(intCh); Serial.print("="); Serial.println(dev[intCh].cursta);
-        microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + String(intCh) + "=" + String(dev[intCh].cursta));
+        //microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + String(intCh) + "=" + String(dev[intCh].cursta));
+        microgear.publish("/chat", String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + String(intCh) + "=" + String(dev[intCh].cursta),true);
         return;
       }
     }
@@ -156,13 +225,17 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
         dev[ch.toInt()].cursta = map(dev[ch.toInt()].cursta, 0, 255, 255, 0);
       }
       else dev[ch.toInt()].cursta = strVal.toInt();
+#ifndef ARDUINO_AS_IO
+      setPinsValue(i);
+#endif
     }
     String bbody = "a=";
     for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++) {
       bbody += dev[i].cursta;
       if (i < sizeof(dev) / sizeof(dev[0]) - 1)bbody += ",";
     }
-    microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + bbody);
+    //microgear.chat(bsender, String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + bbody);
+    microgear.publish("/chat", String(wifiManager.mgear.Alias) + "#" + String(sender) + "#" + bbody,true);
     Serial.println(body);
     return;
   }
@@ -183,18 +256,68 @@ void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
   microgear.setAlias(wifiManager.mgear.Alias);
 }
 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  String sMsg = "";
+  switch (type) {
+    case WStype_DISCONNECTED:
+      // Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        //IPAddress ip = webSocket.remoteIP(num);
+        //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        //webSocket.sendTXT(num, "Connected");
+      }
+      break;
+    case WStype_TEXT:
+      for (int i = 0; i < length; i++)
+        sMsg += (char)payload[i];
+      #ifdef ARDUINO_ESP8266_NODEMCU
+      inputString = sMsg;
+      stringComplete = true;
+      #else
+      Serial.println(sMsg);
+      #endif
+      // send message to client
+      // webSocket.sendTXT(num, "message here");
+
+      // send data to all connected clients
+      // webSocket.broadcastTXT("message here");
+      break;
+    case WStype_BIN:;
+      //Serial.printf("[%u] get binary length: %u\n", num, length);
+      //hexdump(payload, length);
+
+      // send message to client
+      // webSocket.sendBIN(num, payload, length);
+      break;
+  }
+
+}
+
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.println(F("Starting..."));
   Serial.println(F("wait for requirement to clear setting"));
-  pinMode(CLEAR_SETTING_TRICKER, INPUT);
+  pinMode(CLEAR_SETTING_TRICKER, INPUT_PULLUP);
+#ifndef ARDUINO_AS_IO
+  for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++)
+    for (byte j = 0; j < getAvailablePins(i); j++)
+      pinMode(dev[i].pins[j], OUTPUT);
+#endif
   delay(100);
   if (digitalRead(CLEAR_SETTING_TRICKER) == 0) {
     //reset saved settings
     //wifiManager.emptyGear(); //clear gear config
     wifiManager.resetSettings();
     Serial.println(F("Setting has been cleared!"));
+    // tone(D2, 494, 20);
+  } else {
+
   }
+  // tone(D2, 440, 20);
   Serial.println(F("set microgear eepromoffset"));
   microgear.setEEPROMOffset(101);
 
@@ -248,10 +371,18 @@ void setup() {
     Serial.println(F("Microgear was silent! because anything is wrong!\nPlease press reset to reboot"));
   }
 
-  Serial.print(F("Start WebService port:"));
-  Serial.println(String(HTTP_PORT));
+
   initHandleds();
-  server.begin();
+  webServer.begin();
+  Serial.print(F("HTTP PORT:"));
+  Serial.println(String(HTTP_PORT));
+
+  //  socketServer.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.print(F("TCP PORT:"));
+  Serial.println(String(TCP_PORT));
+
   Serial.println(F("done"));
 }
 
@@ -259,10 +390,12 @@ void loop() {
   unsigned long currentMillis = millis();
 
   //still running webserver
-  server.handleClient();
+  webServer.handleClient();
 
+  //still running websocket
+  webSocket.loop();
   //update running status to arduino
-  if (currentMillis - pRunning >= (sta_running_duration/2)) {
+  if (currentMillis - pRunning >= (sta_running_duration / 2)) {
     Serial.println("RUN:1");
     pRunning = currentMillis;
   }
@@ -294,6 +427,34 @@ void loop() {
   //Serial.println("test");
   //delay(1000);
   //other ........... .
+  //serialEvent();
+
+  /*
+    if (!socketClient.connected()) {
+      //try to connect to a new client
+      socketClient = socketServer.available();
+    } else {
+      if (socketClient.available() > 0) {
+        while (socketClient.available()) {
+          byte b = socketClient.read();
+          if (b == '\n') {
+            stringComplete = true;
+            break;
+          }
+
+          inputString += (char)b;
+        }
+        socketClient.flush();
+      }
+    }*/
+
+  if (stringComplete) {
+    processCommand();
+    Serial.println(inputString);
+    //    socketClient.println(inputString);
+    inputString = "";
+    stringComplete = false;
+  }
 }
 
 #endif
--- examples/GearDev/MyFunction.h
+++ examples/GearDev/MyFunction.h
@@ -1,53 +1,191 @@
- #ifndef MYFUNCTION
- #define MYFUNCTION
- byte getAvailablePins(byte pin[]){
-  byte count=0;
-  for(byte i=0;i<sizeof(pin);i++){
-    if(pin[i]>0){
-      count+=1;
-      }
-  }
-  return count;
+#ifndef MYFUNCTION
+#define MYFUNCTION
+void beep(int duration){
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(duration);   
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
 }
- byte getAvailablePins(int id){
-  byte count=0;
-  for(byte i=0;i<sizeof(dev[id].pins)/sizeof(dev[id].pins[0]);i++){
-    if(dev[id].pins[i]>0){
-      count+=1;
-      }
-  }
-  return count;
-}
- String splitString(String data, char separator, int index){
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
+byte getAvailablePins(byte pin[]) {
+  byte count = 0;
+  for (byte i = 0; i < sizeof(pin); i++) {
+    if (pin[i] > 0) {
+      count += 1;
     }
   }
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+  return count;
+}
+byte getAvailablePins(int id) {
+  byte count = 0;
+  for (byte i = 0; i < sizeof(dev[id].pins) / sizeof(dev[id].pins[0]); i++) {
+    if (dev[id].pins[i] > 0) {
+      count += 1;
+    }
+  }
+  return count;
+}
+String splitString(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-boolean isValidNumber(String str){
-   boolean isNum=false;
-   for(byte i=0;i<str.length();i++)
-   {
-       isNum = isDigit(str.charAt(i)) || str.charAt(i) == '+' || str.charAt(i) == '.' || str.charAt(i) == '-';
-       if(!isNum) return false;
-   }
-   return isNum;
+boolean isValidNumber(String str) {
+  boolean isNum = false;
+  for (byte i = 0; i < str.length(); i++)
+  {
+    isNum = isDigit(str.charAt(i)) || str.charAt(i) == '+' || str.charAt(i) == '.' || str.charAt(i) == '-';
+    if (!isNum) return false;
+  }
+  return isNum;
 }
 
-String char2String(uint8_t* msg,unsigned int msglen){
+String char2String(uint8_t* msg, unsigned int msglen) {
   String msgs;
-    for (unsigned int i=0; i<msglen; i++)
-        msgs+=(char)msg[i];
-   return msgs;
+  for (unsigned int i = 0; i < msglen; i++)
+    msgs += (char)msg[i];
+  return msgs;
+}
+
+void setPinsValue(int i) {
+  //dev[i].cursta=intVal;
+#ifdef ARDUINO_AS_IO
+  EEPROM.write(i + PREVIOS_STATUS_ADDRESS, dev[i].cursta);
+#endif
+  byte pin_use = getAvailablePins(i);
+  if (pin_use == 1) {
+    //ถ้าเป็นขาสัญญาณ pwm
+    
+    if (
+  #ifdef ARDUINO_AS_IO
+    #ifdef __AVR_ATmega328P__
+          // ใช้ตัวนี้ถ้าเป็นบอร์ด Arduino ที่ใช้ชิป Atmega328p
+          (dev[i].pins[0] == 3 || dev[i].pins[0] == 5 || dev[i].pins[0] == 6 || dev[i].pins[0] == 9 || dev[i].pins[0] == 10 || dev[i].pins[0] == 11)
+    #elif __AVR_ATmega1280__ || __AVR_ATmega2560__
+          // ใช้ตัวนี้ถ้าเป็นบอร์ด Arduino ที่ใช้ชิป Atmega1280 หรือ Atmega2560
+          ((dev[i].pins[0] >= 2 && dev[i].pins[0] <= 13) || (dev[i].pins[0] > 44 && dev[i].pins[0] <= 46))
+    #else
+          false
+    #endif
+      &&
+  #else
+      //true ||
+  #endif
+      (dev[i].cursta < 255 && dev[i].cursta > 0)) {
+      //ให้ใช้ analogWrite()
+      //Serial.println(dev[i].cursta);
+      analogWrite(dev[i].pins[0], dev[i].cursta);
+
+    } else {
+      //ถ้าไม่ใช่ให้คำนวนค่าที่จะใช้กับ digitalWrite() เนื่องจากค่าที่ส่งมาอยู่ในช่วง 0-255
+      #ifdef ARDUINO_ESP8266_NODEMCU
+      analogWrite(dev[i].pins[0], dev[i].cursta);
+      #else
+      Serial.println(dev[i].cursta);
+      digitalWrite(dev[i].pins[0], map(dev[i].cursta, 0, 255, 0, 1));//use this for active low opto.
+      #endif
+    }
+  } else if (pin_use == 2) {
+    /*chanel นี้ใช้ IO 2 ขา มี 3 status คือ เปิด2/เปิด1/ปิด ลักษณะการกำหนดสถานะของขา IO จออ้างอิงตามการเขียนเลขฐานสอง
+      dev[i].pins[1] | dev[i].pin[0]
+            0                0         =       ปิด
+            0                1         =       เปิด1
+            1                0         =       เปิด2
+            1                1         =       N/A (ไม่ได้กำหนดให้มี status นี้)
+    */
+    if (dev[i].cursta <= 84) {
+      //0 - 84
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+    } else if (dev[i].cursta <= 169) {
+      //85 - 169
+      digitalWrite(dev[i].pins[0], 1);
+      digitalWrite(dev[i].pins[1], 0);
+    } else {
+      //170 - 255
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 1);
+    }
+  } else if (pin_use == 3) {
+    /*chanel นี้ใช้ IO 3 ขา มี 4 status คือ เปิด3/เปิด2/เปิด1/ปิด ลักษณะการกำหนดสถานะของขา IO จออ้างอิงตามการเขียนเลขฐานสอง
+      dev[i].pins[2] | dev[i].pins[1] | dev[i].pin[0]
+            0                0                0         =       ปิด
+            0                0                1         =       เปิด1
+            0                1                0         =       เปิด2
+            1                0                0         =       เปิด3
+            1                1                1         =       N/A (ไม่ได้กำหนดให้มี status นี้)
+    */
+    if (dev[i].cursta <= 63) {
+      //0-63
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 0);
+    } else if (dev[i].cursta <= 127) {
+      //64-127
+      digitalWrite(dev[i].pins[0], 1);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 0);
+    } else if (dev[i].cursta <= 191) {
+      //128-191
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 1);
+      digitalWrite(dev[i].pins[2], 0);
+    } else {
+      //192-255
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 1);
+    }
+  } else if (pin_use == 4) {
+    /*chanel นี้ใช้ IO 4 ขา มี 5 status คือ เปิด4/เปิด3/เปิด2/เปิด1/ปิด ลักษณะการกำหนดสถานะของขา IO จออ้างอิงตามการเขียนเลขฐานสอง
+      dev[i].pins[3] | dev[i].pins[2] | dev[i].pins[1] | dev[i].pin[0]
+            0                0                0                0         =       ปิด
+            0                0                0                1         =       เปิด1
+            0                0                1                0         =       เปิด2
+            0                1                0                0         =       เปิด3
+            1                0                0                0         =       เปิด4
+            1                1                1                1         =       N/A (ไม่ได้กำหนดให้มี status นี้)
+    */
+    if (dev[i].cursta <= 51) {
+      //0-51
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 0);
+      digitalWrite(dev[i].pins[3], 0);
+    } else if (dev[i].cursta <= 102) {
+      //52-102
+      digitalWrite(dev[i].pins[0], 1);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 0);
+      digitalWrite(dev[i].pins[3], 0);
+    } else if (dev[i].cursta <= 153) {
+      //103-153
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 1);
+      digitalWrite(dev[i].pins[2], 0);
+      digitalWrite(dev[i].pins[3], 0);
+    } else if (dev[i].cursta <= 204) {
+      //154-204
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 1);
+      digitalWrite(dev[i].pins[3], 0);
+    } else {
+      //205-255
+      digitalWrite(dev[i].pins[0], 0);
+      digitalWrite(dev[i].pins[1], 0);
+      digitalWrite(dev[i].pins[2], 0);
+      digitalWrite(dev[i].pins[3], 1);
+    }
+  }
 }
 
 void serialEvent() {
@@ -73,4 +211,108 @@ void serialEvent1() {
   }
 }
 #endif
+
+
+void endableRestorePreviosPinState(boolean b) {
+  if (b)EEPROM.write(0, 1);
+  else EEPROM.write(0, 0);
+}
+
+void processCommand() {
+  String body = inputString;
+  if (body.startsWith("a")) {
+    String strVal = splitString(body, '=', 1);
+    if (isValidNumber(strVal)) {
+      int intVal = strVal.toInt();
+      if (intVal >= 0 && intVal <= 255) {
+        for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++) {
+          //chanel นี้ใช้ IO 1 ขา มี 2 status คือ ปิด/เปิด หรือ ถ้ารองรับ pwm ก็สามารถกำหนดค่าให้ได้ในช่วง 0-255
+          dev[i].cursta = intVal;
+          setPinsValue(i);
+        }
+      }
+    }
+  } else if (body.startsWith("enr")) {
+    String strVal = splitString(body, '=', 1);
+    if (strVal.equals("")) {
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+      Serial2.println("enr=" + String(EEPROM.read(0)));
+#endif
+      Serial.println("enr=" + String(EEPROM.read(0)));
+    } else if (strVal.equals("true") || strVal.equals("1")) {
+      endableRestorePreviosPinState(true);
+    } else {
+      endableRestorePreviosPinState(false);
+    }
+  } else if (body.startsWith("msg")) {
+#ifdef ARDUINO_ESP8266_NODEMCU
+    Serial.println("MSG:");
+    body.replace("msg:", "");
+    Serial.println("\t" + body);
+    webSocket.broadcastTXT("Hello" + body);
+#endif
+  } else if (body.startsWith("?")) {
+    body = "a=";
+    for (int i = 0; i < sizeof(dev) / sizeof(dev[0]); i++) {
+      body += dev[i].cursta;
+      if (i < sizeof(dev) / sizeof(dev[0]) - 1)body += ",";
+    }
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    Serial2.println(body);
+#endif
+    Serial.println(body);
+  } else if (body.indexOf("=") > 0) {
+    String strCh = splitString(body, '=', 0);
+    String strVal = splitString(body, '=', 1);
+    if (isValidNumber(strCh)) {
+      int intCh = strCh.toInt();
+      if (isValidNumber(strVal)) {
+        int intVal = strVal.toInt();
+        dev[intCh].cursta = intVal;
+        setPinsValue(intCh);
+      } else if (strVal.equals("!")) {
+        dev[intCh].cursta = map(dev[intCh].cursta, 0, 255, 255, 0);
+        setPinsValue(intCh);
+      }
+    }
+  } else if (body.indexOf(">") > 0) {
+    String strVal = splitString(body, '>', 0);
+    String strCh = splitString(body, '>', 1);
+    int iob[sizeof(dev) / sizeof(dev[0])];
+    int count = 0;
+    for (int i = 0; i < sizeof(iob); i++) {
+      count++;
+      String ch = splitString(strCh, ',', i);
+      if (ch.equals("")) break;
+      if (strVal.equals("!")) {
+        dev[ch.toInt()].cursta = map(dev[ch.toInt()].cursta, 0, 255, 255, 0);
+      } else {
+        dev[ch.toInt()].cursta = strVal.toInt();
+      }
+      setPinsValue(ch.toInt());
+    }
+  }
+#if defined(ARDUINO_AS_IO) &&  defined(__AVR_ATmega328P__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  else if (body.startsWith("RUN:")) {
+    body.replace("RUN:", "");
+    STA = body.toInt();
+  } else if (body.equals("done")) {
+    tone(13, NOTE_CS7, 20);
+    delay(80);
+    tone(13, NOTE_CS7, 20);
+    espboot = true;
+  } else if (body.equals("r")) {
+    //digitalWrite(THIS_RESET_TRICKER, 0);
+    delay(5000);
+    software_Reset();
+  } else {
+#ifndef ARDUINO_ESP8266_NODEMCU
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    Serial2.println("not a command!");
+#endif
+    Serial.println("not a command!");
+#endif
+  }
+#endif
+}
 #endif
